86055954be781b0d7988495f0a04ac4b68a3bf63
Michael Geramb <lasso_riefen0m@icloud.com>
2024-08-15 21:09:41 +0200

    Fix RP2040 configuration and build

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 #ifdef ARDUINO_ARCH_RP2040
     #include "FileTransferModule.h"
     #include "UsbExchangeModule.h"
-    #if defined(KNX_IP_W5500) || defined(KNX_IP_GENERIC) || defined(KNX_IP_WIFI)
-        #include "NetworkModule.h"
-    #endif
 #endif
 #ifdef ARDUINO_ARCH_RP2040
     #pragma message "Pico Core Version: " ARDUINO_PICO_VERSION_STR
@@ -20,9 +17,6 @@ void setup()
  #ifdef ARDUINO_ARCH_RP2040
     openknx.addModule(9, openknxFileTransferModule);
     openknx.addModule(8, openknxUsbExchangeModule);
-    #if defined(KNX_IP_W5500) || defined(KNX_IP_GENERIC) || defined(KNX_IP_WIFI)
-    openknx.addModule(7, openknxNetwork);
-    #endif
 #endif
     openknx.addModule(2, openknxShutterControllerModule);
     openknx.addModule(1, openknxLogic);
@@ -30,14 +24,11 @@ void setup()
 
     Serial.printf("OpenKNX flash size: %lu\r\n", (unsigned long) openknx.openknxFlash.size());
 }
-
-#ifdef tskKERNEL_VERSION_NUMBER
   
 void loop()
 {
     openknx.loop();
 }
-#endif
 
 #ifdef OPENKNX_DUALCORE
 void setup1()
