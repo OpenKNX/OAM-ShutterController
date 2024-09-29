1dafe64f5fb1326b6c44bf62a28d8bff941d4f9f
Michael Geramb <lasso_riefen0m@icloud.com>
2024-09-29 20:28:54 +0200

    upade dependencies

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "c6cfa55"
+#define MAIN_Version "d3d7b5f"
 #define KNX_Version "2.1.0+7efc9a5"
 #define MODULE_Common_Version "1.2.0+8410b2a"
 #define MODULE_Common_ETS 18
@@ -8,5 +8,9 @@
 #define MODULE_LogicModule_ETS 51
 #define MODULE_ShutterControllerModule_Version "0.1.1+dac2652"
 #define MODULE_ShutterControllerModule_ETS 1
+#define MODULE_FileTransferModule_Version "0.0.4+db4a240"
+#define MODULE_FileTransferModule_ETS 0
+#define MODULE_UsbExchange_Version "0.0.1+1ce7a13"
+#define MODULE_UsbExchange_ETS 0
 #define MODULE_FunctionBlocks_Version "0.1.0+181ff20"
 #define MODULE_FunctionBlocks_ETS 1
