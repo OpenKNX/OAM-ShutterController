d38de2f16678c40ba69edf8f90059a164fb66660
Michael Geramb <lasso_riefen0m@icloud.com>
2024-12-28 19:59:32 +0100

    Update dependencies

--- include/versions.h
+++ include/versions.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#define MAIN_Version "3a103cd"
+#define MAIN_Version "3da4aa8"
 #define KNX_Version "2.1.2+d02d04f"
-#define MODULE_FileTransferModule_Version "0.0.5+e39c804"
-#define MODULE_FileTransferModule_ETS 0
 #define MODULE_Common_Version "1.3.0+208e5b5"
 #define MODULE_Common_ETS 19
 #define MODULE_LogicModule_Version "3.5.2+2dbceb4"
 #define MODULE_LogicModule_ETS 53
-#define MODULE_FunctionBlocks_Version "0.3.0-DEV+7e66479"
-#define MODULE_ShutterControllerModule_Version "0.3.0+89852c8"
+#define MODULE_ShutterControllerModule_Version "0.3.1+54e84af"
 #define MODULE_ShutterControllerModule_ETS 3
+#define MODULE_FileTransferModule_Version "0.0.5+e39c804"
+#define MODULE_FileTransferModule_ETS 0
 #define MODULE_UsbExchange_Version "0.0.1+1ce7a13"
 #define MODULE_UsbExchange_ETS 0
+#define MODULE_FunctionBlocks_Version "0.3.0-DEV+f2df2c4"
