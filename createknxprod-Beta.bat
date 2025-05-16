a437aaf6f55277ea5d277b76d91d4f7db6447e84
Michael Geramb <lasso_riefen0m@icloud.com>
2025-05-16 17:35:21 +0200

    Update dependencies, add script for building beta

---
+++ createknxprod-Beta.bat
@@ -0,0 +1,2 @@
+cls
+openknxproducer create ./src/ShutterController.xml -h ./include/knxprod.h -o JalousienSteuerung-Beta.knxprod -debug
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 void setup()
 {
     // change this also in library.json
-    const uint8_t firmwareRevision = 2;
+    const uint8_t firmwareRevision = 5;
     openknx.init(firmwareRevision);
 #if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP32)
 #ifndef OPENKNX_FILE_TRANSFER_IGNORE
